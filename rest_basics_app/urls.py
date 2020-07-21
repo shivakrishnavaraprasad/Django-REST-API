0cb22e77dfba874b339a252d7888ee4bcc2ca4ba
shivakrishna <shivakrishnavaraprasad@outlook.com>
2020-07-21 12:11:59 +0530

    update APIViews

--- rest_basics_app/urls.py
+++ rest_basics_app/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from . views import article_list, article_detail
+from . views import article_list, article_detail, ArticleAPIViews
 
 urlpatterns = [
-    path('articles/', article_list, name='article_list'),
-    path('detail/<int:pk>/', article_detail, name='article_detail'),
-    path('articles/<int:pk>/', article_detail, name='article_detail'),
+    path('article/', ArticleAPIViews.as_view(), name='Article_APIViews'),
+    path('article/<int:id>/', ArticleAPIViews.as_view(), name='Article_detail'),
 ]
+
