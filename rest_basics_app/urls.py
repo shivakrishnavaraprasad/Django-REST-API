240346c76ca402249bb28be0b91ca15d2bee880d
shivakrishna <shivakrishnavaraprasad@outlook.com>
2020-07-22 13:06:39 +0530

    bug fix at urls.py

--- rest_basics_app/urls.py
+++ rest_basics_app/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from . views import article_list, article_detail, ArticleAPIViews
+from . views import article_list, article_detail
 
 urlpatterns = [
-    path('article/', ArticleAPIViews.as_view(), name='Article_APIViews'),
-    path('article/<int:id>/', ArticleAPIViews.as_view(), name='Article_detail'),
+    path('article/', article_list, name='article_list'),
+    path('article/<int:id>/',article_detail, name='Article_detail'),
 ]
 
