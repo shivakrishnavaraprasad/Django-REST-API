ef5a29e7605faf2b961922824b9f3403b1a02603
shivakrishna <shivakrishnavaraprasad@outlook.com>
2020-07-23 12:46:03 +0530

    update generic views

--- rest_basics_app/urls.py
+++ rest_basics_app/urls.py
@@ -2,10 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . views import ArticleDetail, ArticleAPIViews, GenericApiView
 
-urlpatterns = [
-    path('article/', ArticleAPIViews.as_view(), name='Article_APIViews'),
-    path('article/<int:id>/', ArticleAPIViews.as_view(), name='Article_detail'),
-    
+urlpatterns = [    
     # for get | post
     path('generic/article/', GenericApiView.as_view(), name='generic_views'), 
     # for put | delete
