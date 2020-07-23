c53f468f0c29aa5f1cf655ff829086f218850d4a
shivakrishna <shivakrishnavaraprasad@outlook.com>
2020-07-23 20:23:06 +0530

    generic viewsets first commit

--- rest_basics_app/views.py
+++ rest_basics_app/views.py
@@ -15,69 +15,12 @@ from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
 
 
-class ArticleViewSet(viewsets.ViewSet):
-    def list(self, request):
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        queryset = Article.objects.all()
-        article = get_object_or_404(queryset, pk=pk)
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data) 
-
-    def create(self, request):
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, pk=None):
-        article = Article.objects.get(pk=pk)
-        serializer = ArticleSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-    
-
-
-
-
-
-################### generic api views #################################
-class GenericApiView(generics.GenericAPIView, mixins.ListModelMixin,
-                                              mixins.CreateModelMixin, 
-                                              mixins.UpdateModelMixin,
+class ArticleViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
                                               mixins.RetrieveModelMixin,
+                                              mixins.CreateModelMixin,
+                                              mixins.UpdateModelMixin,
                                               mixins.DestroyModelMixin,):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
-    lookup_field = 'id'
 
-    # login authentication
-    # authentication_classes = [SessionAuthentication, BasicAuthentication]
-
-    # Token authentication
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, id=None):
-        if id:
-            return self.retrieve(request)
-        else:
-            return self.list(request)
-
-    def post(self, request):
-        return self.create(request)
-
-    def put(self, request, id=None):
-        return self.update(request, id)
-    
-    def delete(self, request, id):
-        return self.destroy(request, id)
