e72a00072bcbe47546d4ef387d434436d3e9d9d7
shivakrishna <shivakrishnavaraprasad@outlook.com>
2020-07-23 12:40:36 +0530

    commit generic views

--- rest_basics_app/urls.py
+++ rest_basics_app/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from . views import ArticleDetail, ArticleAPIViews
+from . views import ArticleDetail, ArticleAPIViews, GenericApiView
 
 urlpatterns = [
     path('article/', ArticleAPIViews.as_view(), name='Article_APIViews'),
     path('article/<int:id>/', ArticleAPIViews.as_view(), name='Article_detail'),
+    
+    # for get | post
+    path('generic/article/', GenericApiView.as_view(), name='generic_views'), 
+    # for put | delete
+    path('generic/article/<int:id>/', GenericApiView.as_view(), name='generic_views'),
 ]
 
--- rest_basics_app/views.py
+++ rest_basics_app/views.py
@@ -12,6 +12,37 @@ from rest_framework import generics
 from rest_framework import mixins
 
 
+################### generic api views #################################
+class GenericApiView(generics.GenericAPIView, mixins.ListModelMixin,
+                                              mixins.CreateModelMixin, 
+                                              mixins.UpdateModelMixin,
+                                              mixins.RetrieveModelMixin,
+                                              mixins.DestroyModelMixin,):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+    lookup_field = 'id'
+
+    def get(self, request, id=None):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    
+    def delete(self, request, id=None):
+        return self.destroy(request, id)
+
+
+
+
+
+
+
+
 
 ################### class based views - api views #################################
 class ArticleAPIViews(APIView):
